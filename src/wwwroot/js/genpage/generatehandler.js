d4b1bbee12408afd62b23eaf1a48edfe619b45a0
Aimeri Baddouh <aimeri@baddouh.me>
2024-07-26 12:38:39 -0400

    Update Mobile UI
    This update modifies the layout slightly to swap current_image and batch_images. This allows current_images to be rendered at the top in a single column layout.
    
    It introduces a mobile script to handle mobile interactions and detection
    
    Updates the associated scripts to handle correct sizing and eventing on mobile screens
    
    Update the css stylesheets to account for smaller screen sizes

--- src/wwwroot/js/genpage/generatehandler.js
+++ src/wwwroot/js/genpage/generatehandler.js
@@ -51,6 +51,9 @@ class GenerateHandler {
 
     doInterrupt(allSessions = false) {
         this.interrupted = this.batchesEver;
+        if(isLikelyMobile()){
+            document.getElementById('alt_prompt_region').scrollIntoView({ behavior: 'smooth', block: 'start' });
+        }
         doInterrupt(allSessions);
     }
 
@@ -100,6 +103,9 @@ class GenerateHandler {
             delete input_overrides['_preview'];
         }
         this.beforeGenRun();
+        if(isLikelyMobile()){
+            document.getElementById("current_image_batch").scrollIntoView({ behavior: 'smooth', block: 'start' });
+        }
         let run = () => {
             this.resetBatchIfNeeded();
             let images = {};
@@ -168,6 +174,9 @@ class GenerateHandler {
                         }
                         discardable[data.batch_index] = images[data.batch_index];
                         delete images[data.batch_index];
+                        if(isLikelyMobile()){
+                            document.getElementById('current_image').scrollIntoView({ behavior: 'smooth', block: 'start' });
+                        }
                     }
                 }
                 if (data.gen_progress) {
